b12f67c62372628f1cff75f34e4e5e9b6413e90f
unknown <howell2028@yahoo.com>
2023-03-27 14:32:32 +0100

    refactor

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 SECRET_KEY = os.urandom(32)
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:professor9@localhost:5432/flashcard'
+SQLALCHEMY_DATABASE_URI = 'postgresql://react_flashcard_db_user:1Dq3GrKGaW0hqtBspNLMOaPiaLa4AZed@dpg-cggo1l9mbg5e1o04gj20-a.oregon-postgres.render.com/react_flashcard_db'
+# os.environ.get('DATABASE_URL')
+# postgresql://postgres:professor9@localhost:5432/flashcard
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = os.urandom(64)
