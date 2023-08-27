69269d828ee0efc468551df81861c248c731cae0
Calistus Obeke <howell2028@yahoo.com>
2023-08-27 16:29:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 SECRET_KEY = os.urandom(32)
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://react_flashcard_db_user:1Dq3GrKGaW0hqtBspNLMOaPiaLa4AZed@dpg-cggo1l9mbg5e1o04gj20-a.oregon-postgres.render.com/react_flashcard_db'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://react_flashcard_db_user:1Dq3GrKGaW0hqtBspNLMOaPiaLa4AZed@dpg-cggo1l9mbg5e1o04gj20-a.oregon-postgres.render.com/react_flashcard_db'
+SQLALCHEMY_DATABASE_URI = 'postgres://flashcard_vti0_user:Rr0R6GU1RqD7ozEE8B0XzmSDA5iRFMKO@dpg-cjljgk8cfp5c73a5u0og-a.oregon-postgres.render.com/flashcard_vti0'
 # os.environ.get('DATABASE_URL')
 # postgresql://postgres:professor9@localhost:5432/flashcard
 SQLALCHEMY_TRACK_MODIFICATIONS = False
